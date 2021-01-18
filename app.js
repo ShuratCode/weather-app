da9614a82c83307b5c52be466d27e06219b6c673
Shaked Eyal <shaked.eyal@protonmail.com>
2021-01-18 18:51:41 +0200

    use postman-request build in json parser

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const request = require('postman-request')
 const api_key = process.env.api_key
 const url = 'http://api.weatherstack.com/current?access_key=' + api_key + '&query=37.8267,-122.4233'
 
-request(url, {}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request(url, {json: true}, (error, response) => {
+    console.log(response.body.current)
 })
