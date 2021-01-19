078ab51f81af6408c82ebf0c552877c75a42e770
Shaked Eyal <shaked.eyal@protonmail.com>
2021-01-19 10:51:39 +0200

    change unit to f and added weather descriptions

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const request = require('postman-request')
 const api_key = process.env.api_key
-const url = 'http://api.weatherstack.com/current?access_key=' + api_key + '&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=' + api_key + '&query=37.8267,-122.4233&units=f'
 
 request(url, {json: true}, (error, response) => {
-    console.log(response.body.current)
+    const degrees = response.body.current.temperature
+    const feels_like = response.body.current.feelslike
+    console.log(response.body.current.weather_descriptions[0] + ". It is currently " + degrees + " out. It feels like " + feels_like + " degrees out.")
 })
