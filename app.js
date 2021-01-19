107cbab00b16ad9eb46351eb595d01c9a3d21393
Shaked Eyal <shaked.eyal@protonmail.com>
2021-01-19 11:31:16 +0200

    added geo coding api request

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const request = require('postman-request')
+const { exit } = require('process')
 const api_key = process.env.api_key
-const url = 'http://api.weatherstack.com/current?access_key=' + api_key + '&query=37.8267,-122.4233&units=f'
+const weatherstackURL = 'http://api.weatherstack.com/current?access_key=' + api_key + '&query=37.8267,-122.4233&units=f'
 
-request(url, {json: true}, (error, response) => {
+request(weatherstackURL, {json: true}, (error, response) => {
     const degrees = response.body.current.temperature
     const feels_like = response.body.current.feelslike
     console.log(response.body.current.weather_descriptions[0] + ". It is currently " + degrees + " out. It feels like " + feels_like + " degrees out.")
+})
+
+const access_token=process.env.access_key
+const geocodingURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=' + access_token + '&limit=1'
+request(geocodingURL, {json:true}, (error, response) => {
+    if (error)
+    {
+        console.log(error)
+        exit(1)
+    }
+    const features = response.body.features
+    if (features.length > 0)
+    {
+        const latitude = features[0].center[1]
+        const longitude = features[0].center[0]
+        console.log("The coordinates for your location are: " + latitude + ',' + longitude)
+    }
 })
